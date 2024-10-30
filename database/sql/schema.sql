014d05fb6bc26ef379b93c5f422175c84b6f83f2
Daniel Harrison <98435341+Soakar2@users.noreply.github.com>
2024-10-30 11:14:12 -0500

    Update schema.sql

--- database/sql/schema.sql
+++ database/sql/schema.sql
@@ -199,59 +199,97 @@ CREATE TABLE IF NOT EXISTS "trackman_batter" (
 );
 
 CREATE TABLE IF NOT EXISTS "practice_trackman_metadata" (
-  "PitchUID" uuid UNIQUE PRIMARY KEY DEFAULT (uuid_generate_v4()),
-  "GameDate" date,
-  "PitchTime" time,
-  "Inning" int CHECK ("Inning" > 0),
-  "TopBottom" varchar CHECK ("TopBottom" IN ('Top', 'Bottom')),
-  "Outs" int CHECK ("Outs" BETWEEN 0 AND 2),
-  "Balls" int CHECK ("Balls" BETWEEN 0 AND 3),
-  "Strikes" int CHECK ("Strikes" BETWEEN 0 AND 2),
-  "PitchCall" varchar,
-  "KorBB" varchar CHECK ("KorBB" IN ('K', 'BB', 'HBP', 'Other')),
-  "TaggedHitType" varchar,
-  "PlayResult" varchar,
-  "OutsOnPlay" int CHECK ("OutsOnPlay" BETWEEN 0 AND 3),
-  "RunsScored" int CHECK ("RunsScored" >= 0),
-  "RunnersAt" varchar,
-  "HomeTeam" varchar REFERENCES "teams" ("TeamName") ON DELETE CASCADE,
-  "AwayTeam" varchar REFERENCES "teams" ("TeamName") ON DELETE CASCADE,
-  "Stadium" varchar,
-  "Level" varchar,
-  "League" varchar,
-  "GameID" varchar UNIQUE,
-  "GameUID" varchar UNIQUE,
-  "UTCDate" date,
-  "UTCtime" time,
-  "LocalDateTime" timestamp,
-  "UTCDateTime" timestamp,
-  "AutoHitType" varchar,
-  "System" varchar,
-  "HomeTeamForeignID" varchar,
-  "AwayTeamForeignID" varchar,
-  "GameForeignID" varchar,
-  "PlayID" varchar
+    "PitchUID" uuid UNIQUE PRIMARY KEY DEFAULT (uuid_generate_v4()),
+    "GameDate" date,
+    "PitchTime" time,
+    "Inning" int,
+    "TopBottom" varchar,
+    "Outs" int,
+    "Balls" int,
+    "Strikes" int,
+    "PitchCall" varchar,
+    "KorBB" varchar,
+    "TaggedHitType" varchar,
+    "PlayResult" varchar,
+    "OutsOnPlay" varchar,
+    "RunsScored" varchar,
+    "RunnersAt" varchar,
+    "HomeTeam" varchar,
+    "AwayTeam" varchar,
+    "Stadium" varchar,
+    "Level" varchar,
+    "League" varchar,
+    "GameID" varchar,
+    "GameUID" varchar,
+    "UTCDate" date,
+    "UTCtime" time,
+    "LocalDateTime" date,
+    "UTCDateTime" date,
+    "AutoHitType" varchar,
+    "System" varchar,
+    "HomeTeamForeignID" varchar,
+    "AwayTeamForeignID" varchar,
+    "GameForeignID" varchar,
+    "PlayID" varchar
 );
 
 CREATE TABLE IF NOT EXISTS "practice_pitching_data" (
   "PitchUID" uuid UNIQUE PRIMARY KEY DEFAULT (uuid_generate_v4()),
   "PitchNo" int,
+  "PAofInning" int,
+  "PitchofPA" int,
   "Pitcher" varchar,
   "PitcherID" int,
   "PitcherThrows" varchar,
   "PitcherTeam" varchar,
+  "PitcherSet" varchar,
   "TaggedPitchType" varchar,
   "AutoPitchType" varchar,
   "RelSpeed" decimal,
-  "InducedVert" decimal,
-  "HorzBreak" decimal,
+  "VertRelAngle" decimal,
+  "HorzRelAngle" decimal,
   "SpinRate" decimal,
   "SpinAxis" decimal,
+  "Tilt" varchar,
+  "RelHeight" decimal,
+  "RelSide" decimal,
+  "Extension" decimal,
+  "VertBreak" decimal,
+  "InducedVert" decimal,
+  "HorzBreak" decimal,
   "PlateLocHeight" decimal,
   "PlateLocSide" decimal,
-  "PitchTime" time,
-  "PracticeSessionID" varchar,
-  "Season" varchar DEFAULT '2024'
+  "ZoneSpeed" decimal,
+  "VertApprAngle" decimal,
+  "HorzApprAngle" decimal,
+  "ZoneTime" decimal,
+  "pfxx" decimal,
+  "pfxz" decimal,
+  "x0" decimal,
+  "y0" decimal,
+  "z0" decimal,
+  "vx0" decimal,
+  "vy0" decimal,
+  "vz0" decimal,
+  "ax0" decimal,
+  "ay0" decimal,
+  "az0" decimal,
+  "SpeedDrop" decimal,
+  "PitchLastMeasuredX" decimal,
+  "PitchLastMeasuredY" decimal,
+  "PitchLastMeasuredZ" decimal,
+  "PitchTrajectoryXc0" decimal,
+  "PitchTrajectoryXc1" decimal,
+  "PitchTrajectoryXc2" decimal,
+  "PitchTrajectoryYc0" decimal,
+  "PitchTrajectoryYc1" decimal,
+  "PitchTrajectoryYc2" decimal,
+  "PitchTrajectoryZc0" decimal,
+  "PitchTrajectoryZc1" decimal,
+  "PitchTrajectoryZc2" decimal,
+  "PitchReleaseConfidence" varchar,
+  "PitchLocationConfidence" varchar,
+  "PitchMovementConfidence" varchar
 );
 
 CREATE TABLE IF NOT EXISTS "practice_batting_data" (
@@ -277,7 +315,36 @@ CREATE TABLE IF NOT EXISTS "practice_batting_data" (
   "ContactPositionX" decimal,
   "ContactPositionY" decimal,
   "ContactPositionZ" decimal,
-  "HitSpinAxis" decimal
+  "HitSpinAxis" decimal,
+  "HitTrajectoryXc0" decimal,
+  "HitTrajectoryXc1" decimal,
+  "HitTrajectoryXc2" decimal,
+  "HitTrajectoryXc3" decimal,
+  "HitTrajectoryXc4" decimal,
+  "HitTrajectoryXc5" decimal,
+  "HitTrajectoryXc6" decimal,
+  "HitTrajectoryXc7" decimal,
+  "HitTrajectoryXc8" decimal,
+  "HitTrajectoryYc0" decimal,
+  "HitTrajectoryYc1" decimal,
+  "HitTrajectoryYc2" decimal,
+  "HitTrajectoryYc3" decimal,
+  "HitTrajectoryYc4" decimal,
+  "HitTrajectoryYc5" decimal,
+  "HitTrajectoryYc6" decimal,
+  "HitTrajectoryYc7" decimal,
+  "HitTrajectoryYc8" decimal,
+  "HitTrajectoryZc0" decimal,
+  "HitTrajectoryZc1" decimal,
+  "HitTrajectoryZc2" decimal,
+  "HitTrajectoryZc3" decimal,
+  "HitTrajectoryZc4" decimal,
+  "HitTrajectoryZc5" decimal,
+  "HitTrajectoryZc6" decimal,
+  "HitTrajectoryZc7" decimal,
+  "HitTrajectoryZc8" decimal,
+  "HitLaunchConfidence" varchar,
+  "HitLandingConfidence" varchar
 );
 
 CREATE TABLE IF NOT EXISTS "practice_catching_data" (
@@ -295,7 +362,22 @@ CREATE TABLE IF NOT EXISTS "practice_catching_data" (
   "CatchPositionZ" decimal,
   "ThrowPositionX" decimal,
   "ThrowPositionY" decimal,
-  "ThrowPositionZ" decimal
+  "ThrowPositionZ" decimal,
+  "BasePositionX" decimal,
+  "BasePositionY" decimal,
+  "BasePositionZ" decimal,
+  "ThrowTrajectoryXc0" decimal,
+  "ThrowTrajectoryXc1" decimal,
+  "ThrowTrajectoryXc2" decimal,
+  "ThrowTrajectoryYc0" decimal,
+  "ThrowTrajectoryYc1" decimal,
+  "ThrowTrajectoryYc2" decimal,
+  "ThrowTrajectoryZc0" decimal,
+  "ThrowTrajectoryZc1" decimal,
+  "ThrowTrajectoryZc2" decimal,
+  "CatcherThrowCatchConfidence" varchar,
+  "CatcherThrowReleaseConfidence" varchar,
+  "CatcherThrowLocationConfidence" varchar
 );
 
 
@@ -347,6 +429,24 @@ CREATE TABLE IF NOT EXISTS "pitcher_run_values" (
   "Score" decimal,
   PRIMARY KEY ("Pitcher", "PitcherTeam", "PitchType")
 );
+
+
+-- Foreign key constraints for practice_trackman_metadata
+ALTER TABLE "practice_trackman_metadata" ADD CONSTRAINT "practice_trackman_metadata_HomeTeam_fkey" FOREIGN KEY ("HomeTeam") REFERENCES "teams" ("TeamName") ON DELETE CASCADE;
+
+ALTER TABLE "practice_trackman_metadata" ADD CONSTRAINT "practice_trackman_metadata_AwayTeam_fkey" FOREIGN KEY ("AwayTeam") REFERENCES "teams" ("TeamName") ON DELETE CASCADE;
+
+ALTER TABLE "practice_pitching_data" ADD CONSTRAINT "practice_pitching_data_Pitcher_PitcherTeam_fkey" FOREIGN KEY ("Pitcher", "PitcherTeam") REFERENCES "players" ("PlayerName", "TeamName") ON DELETE CASCADE;
+
+ALTER TABLE "practice_batting_data" ADD CONSTRAINT "practice_batting_data_Batter_BatterTeam_fkey" FOREIGN KEY ("Batter", "BatterTeam") REFERENCES "players" ("PlayerName", "TeamName") ON DELETE CASCADE;
+
+ALTER TABLE "practice_catching_data" ADD CONSTRAINT "practice_catching_data_Catcher_CatcherTeam_fkey" FOREIGN KEY ("Catcher", "CatcherTeam") REFERENCES "players" ("PlayerName", "TeamName") ON DELETE CASCADE;
+
+ALTER TABLE "practice_pitching_data" ADD CONSTRAINT "practice_pitching_data_PitchUID_fkey" FOREIGN KEY ("PitchUID") REFERENCES "practice_trackman_metadata" ("PitchUID") ON DELETE CASCADE;
+
+ALTER TABLE "practice_batting_data" ADD CONSTRAINT "practice_batting_data_PitchUID_fkey" FOREIGN KEY ("PitchUID") REFERENCES "practice_trackman_metadata" ("PitchUID") ON DELETE CASCADE;
+
+ALTER TABLE "practice_catching_data" ADD CONSTRAINT "practice_catching_data_PitchUID_fkey" FOREIGN KEY ("PitchUID") REFERENCES "practice_trackman_metadata" ("PitchUID") ON DELETE CASCADE;
 
 ALTER TABLE "trackman_batter" ADD CONSTRAINT "trackman_batter_PitchUID_fkey1" FOREIGN KEY ("PitchUID") REFERENCES "trackman_metadata" ("PitchUID") ON DELETE CASCADE;
 
